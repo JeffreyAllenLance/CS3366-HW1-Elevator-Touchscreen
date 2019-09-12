fa76e4582ed2317250074889f03d192bb89b8b28
Jeffrey Lance <jeffreyallenlance@gmail.com>
2019-09-12 02:51:24 -0500

    Upload p5.js and ElevatorTouchsreen.js

---
+++ _layouts/ElevatorTouchscreen.js
@@ -0,0 +1,50 @@
+    let screen, groundButton, groundButtonPressed;
+    let screenWidth=431, screenHeight=632;
+    let groundButtonX, groundButtonY;
+    let groundButtonWidth=253, groundButtonHeight=57;
+    let groundButtonOver = false;
+    let clickedGround = false;
+
+    function preload(){
+        screen = loadImage("https://raw.githubusercontent.com/JeffreyAllenLance/Elevator-Touchscreen/master/Images/Elevator%20Screen.jpg");
+        groundButton = loadImage("https://raw.githubusercontent.com/JeffreyAllenLance/Elevator-Touchscreen/master/Images/Ground%20Floor.jpg");
+        groundButtonPressed = loadImage("https://raw.githubusercontent.com/JeffreyAllenLance/Elevator-Touchscreen/master/Images/Ground%20Floor%20Pressed.jpg");
+    }
+
+    function setup() {
+        createCanvas(431, 632);
+        groundButtonX = 0;
+        groundButtonY = 400;
+    }
+
+    function draw() {
+        background(0);
+        update();
+        image(screen,0,0, screenWidth,screenHeight);
+        if(clickedGround){
+            image(groundButtonPressed, 0, 400, groundButtonWidth, groundButtonHeight);
+        }
+        else {
+            image(groundButton, 0, 400, groundButtonWidth, groundButtonHeight);
+        }
+    }
+    function update() {
+        if ( isOverGroundButton(groundButtonX, groundButtonY, groundButtonWidth, groundButtonHeight) ) {
+            groundButtonOver = true;
+        } else {
+            groundButtonOver = false;
+        }
+    }
+    function mousePressed(){
+        if(groundButtonOver){
+            clickedGround = true;
+        }
+    }
+    function isOverGroundButton(x, y, width, height)  {
+        if (mouseX >= x && mouseX <= x+width &&
+                mouseY >= y && mouseY <= y+height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
